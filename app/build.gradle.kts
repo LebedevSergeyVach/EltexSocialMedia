f8a4ed1a63d37d03a784ee709c127de4718b0711
Lebedev Sergey <sergeylebedevvach@gmail.com>
2024-11-01 16:41:44 +0700

    Main: Add event_card_option_1.xml & post_card_option_1.xml Update MainActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,40 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.ui.tooling.preview.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    /*
+         Constraintlayout
+         https://developer.android.com/reference/androidx/constraintlayout/widget/ConstraintLayout
+    */
+    implementation(libs.androidx.constraintlayout.v220)
+
+    // Compose
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose)
+
+    // Compose LiveData
+    implementation(libs.androidx.runtime.livedata)
+
+    // Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Compose Preview
+    debugImplementation(libs.androidx.ui.tooling)
+
+    implementation(libs.androidx.core.ktx.v160)
+    implementation(libs.androidx.appcompat.v131)
+    implementation(libs.material.v140)
+
+    // Другие зависимости
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit.v113)
+    androidTestImplementation(libs.androidx.espresso.core.v340)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.5")
 }
--- app/src/main/java/com/eltex/androidschool/MainActivity.kt
+++ app/src/main/java/com/eltex/androidschool/MainActivity.kt
@@ -10,30 +10,60 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var helloAppText: TextView
+    private lateinit var author: TextView
+    private lateinit var avatar: TextView
+    private lateinit var dataPublication: TextView
+    private lateinit var optionConducting: TextView
+    private lateinit var dataEvent: TextView
+    private lateinit var content: TextView
+    private lateinit var link: TextView
+    private lateinit var like: TextView
+    private lateinit var peoples: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.event_card_option_1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        helloAppText = findViewById(R.id.hello_app_text)
-        textDisplayView(helloAppText, R.string.eltex, 50)
+        author = findViewById(R.id.author)
+        avatar = findViewById(R.id.authorInitials)
+        dataPublication = findViewById(R.id.dataPlacement)
+        optionConducting = findViewById(R.id.optionConducting)
+        dataEvent = findViewById(R.id.dataEvent)
+        content = findViewById(R.id.content)
+        link = findViewById(R.id.link)
+        like = findViewById(R.id.like)
+        peoples = findViewById(R.id.peoples)
+
+        textDisplayView(author, R.string.testAuthor)
+        textDisplayView(avatar, R.string.testAvatar)
+        textDisplayView(dataPublication, R.string.testDataPlacement)
+        textDisplayView(dataEvent, R.string.testDataEvent)
+        textDisplayView(content, R.string.testContent)
+        textDisplayView(link, R.string.testLink)
+        textDisplayView(like, R.string.testLike)
+        textDisplayView(peoples, R.string.testPeoples)
+    }
+
+
+    private fun textDisplayView(textView: TextView, stringIdText: Int) {
+        textView.text = getString(stringIdText)
     }
 
     private fun textDisplayView(
         textView: TextView,
         stringIdText: Int,
-        sizeText: Int
+        colorIdText: Int,
+        textSize: Int
     ) {
         textView.text = getString(stringIdText)
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, sizeText.toFloat())
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize.toFloat())
+        textView.setTextColor(resources.getColor(colorIdText))
         textView.gravity = Gravity.CENTER
     }
 }
