2e73c15e59842cfce3163a6e9e21c90a8e32c7b4
Lebedev Sergey <sergeylebedevvach@gmail.com>
2024-11-18 10:55:22 +0700

    Main: Update utils/ContextExtensions.kt (Updated the appearance of Toast)

--- app/src/main/java/com/eltex/androidschool/utils/ContextExtensions.kt
+++ app/src/main/java/com/eltex/androidschool/utils/ContextExtensions.kt
@@ -1,19 +1,38 @@
+@file:Suppress("DEPRECATION")
+
 package com.eltex.androidschool.utils
 
-import android.content.Context
-import android.widget.Toast
+import android.annotation.SuppressLint
 import androidx.annotation.StringRes
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import com.eltex.androidschool.R
 
 /**
- * Создаёт и показывает Toast
+ * Создаёт и показывает кастомный Toast
+ *
  * @param res – ссылка на строковый ресурс, который показываем
  * @param short - если true, то Toast.LENGTH_SHORT, иначе Toast.LENGTH_LONG
  */
+@SuppressLint("InflateParams")
 fun Context.toast(
     @StringRes res: Int,
     short: Boolean = true
 ) {
-    Toast.makeText(
-        this, res, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
-    ).show()
+//    Toast.makeText(
+//        this, res, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
+//    ).show()
+
+    val inflater = LayoutInflater.from(this)
+    val layout = inflater.inflate(R.layout.custom_toast, null)
+
+    val textView = layout.findViewById<TextView>(R.id.toast_text)
+    textView.text = getString(res)
+
+    val toast = Toast(this)
+    toast.duration = if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
+    toast.view = layout
+    toast.show()
 }
